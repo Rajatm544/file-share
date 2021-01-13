d1e1f02af0de5a68317a2d48f32181f0ff75685c
Rajatm544 <rajatm544@gmail.com>
2021-01-13 23:42:48 +0530

    set up express app

--- server-side/server.js
+++ server-side/server.js
@@ -13,5 +13,21 @@ app.use(express.json());
 // configure the port for the server side
 const PORT = process.env.PORT || 5000;
 
+// configure the mongoose setup
+const uri = process.env.MONGO_URI;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("MongoDB connection has been established");
+});
+
+// configure the routes
+const fileRouter = require("./routes/file.route");
+app.use("/file", fileRouter);
+
 // host the app on port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
