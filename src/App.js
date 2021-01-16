9df92a0358696d52343fb94d3745ed6f1a14da26
Rajatm544 <rajatm544@gmail.com>
2021-01-16 20:37:24 +0530

    add the main react app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Download from "./components/Download";
 import "./App.css";
 
-function App() {
-    return <div>Frontend goes here</div>;
-}
+const App = () => {
+    return (
+        <div>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route
+                        path="/file/download/:id"
+                        exact
+                        component={Download}
+                    />
+                </Switch>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
