0bcc470e2e20ae4353852e83be4a24378e7d528a
Rajatm544 <rajatm544@gmail.com>
2021-01-21 23:56:38 +0530

    add basic style, a progress bar functionality, fix bugs withh unsupported file types

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,69 +1,150 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import Dropzone from "react-dropzone";
 import axios from "axios";
 import download from "downloadjs";
+import M from "materialize-css";
+import "../stylesheets/home.css";
 
 const Home = (props) => {
+    const dropRef = useRef(null);
+    const submitBtn = useRef(null);
+    const finalLinkRef = useRef(null);
+    const firstRender = useRef(true);
+    const previewImgRef = useRef(null);
+    const [errorMsg, setErrorMsg] = useState("");
     const [file, setFile] = useState({});
-    const [state, setState] = useState({
-        title: "",
-        description: "",
-    });
-    const [allFiles, setAllFiles] = useState([]);
+    const [previewSrc, setPreviewSrc] = useState("");
+    const [isPreviewAvailable, setIsPreviewAvailable] = useState(false);
+    const [uploadedFile, setUploadedFile] = useState({});
+    const [progress, setProgress] = useState(0);
+    const [displayProgress, setDisplayProgress] = useState(false);
+    const [displayLinks, setDisplayLinks] = useState(false);
 
     useEffect(() => {
-        axios
-            .get("http://localhost:5000/file")
-            .then((allFiles) => setAllFiles([...allFiles.data]))
-            .catch((err) => console.error(err));
-    }, []);
+        if (progress < 100 && displayProgress) {
+            window.setTimeout(() => {
+                setProgress(progress + 1);
+            }, 50);
+        } else {
+            if (uploadedFile._id && progress >= 99) {
+                window.setTimeout(() => {
+                    setDisplayProgress(false);
+                    setProgress(0);
+                    setDisplayLinks(true);
+                    window.setTimeout(() => {
+                        finalLinkRef.current.style.opacity = "1";
+                    }, 100);
+                }, 1000);
+            }
+            if (progress !== 0 && !uploadedFile._id) {
+                setProgress(99);
+            }
+        }
+    }, [progress, displayProgress, uploadedFile]);
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setState({
-            ...state,
-            [name]: value,
-        });
+    useEffect(() => {
+        if (firstRender.current) {
+            firstRender.current = false;
+            return;
+        } else {
+            if (isPreviewAvailable && previewSrc) {
+                previewImgRef.current.style.opacity = "1";
+            }
+            if (!isPreviewAvailable && file.name) {
+                dropRef.current.style.minHeight = "50vh";
+                dropRef.current.style.border = "2px dashed #08a1c4";
+                dropRef.current.style.background = "";
+                dropRef.current.style.color = "";
+            } else if (isPreviewAvailable && file.name) {
+                dropRef.current.style.minHeight = "25vh";
+                dropRef.current.style.border = "2px dashed #08a1c4";
+                dropRef.current.style.background = "";
+                dropRef.current.style.color = "";
+            }
+        }
+    }, [isPreviewAvailable, previewSrc, file]);
+
+    useEffect(() => {
+        if (file.name) {
+            setErrorMsg("");
+            if (submitBtn.current) {
+                submitBtn.current.style.visibility = "visible";
+                submitBtn.current.style.opacity = "1";
+            }
+        }
+    }, [file]);
+
+    const updateBorder = (dragState) => {
+        if (dragState === "over") {
+            dropRef.current.style.border = "2px solid #02B875";
+            dropRef.current.style.background =
+                "linear-gradient(315deg, #08a1c4 0%, #08cfbe 40%, #02b875 100%)";
+            dropRef.current.style.color = "white";
+        } else if (dragState === "leave") {
+            dropRef.current.style.border = "2px dashed #08a1c4";
+            dropRef.current.style.background = "";
+            dropRef.current.style.color = "";
+        }
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        try {
-            const { title, description } = state;
-
-            if (title.trim() && description.trim()) {
-                const formdata = new FormData();
-                formdata.append("title", title.trim());
-                formdata.append("description", description.trim());
-                formdata.append("file", file);
-
-                axios
-                    .post("http://localhost:5000/file/", formdata, {
-                        headers: {
-                            "Content-Type": "multipart/form-data",
-                        },
-                    })
-                    .then((data) => console.log(data))
-                    .catch((err) => console.error(err));
-            }
-        } catch (error) {
-            if (error) console.error(error.message);
+        submitBtn.current.style.opacity = "0";
+        if (file) {
+            const formdata = new FormData();
+            formdata.append("file", file);
+            axios
+                .post("http://localhost:5000/file/", formdata, {
+                    headers: {
+                        "Content-Type": "multipart/form-data",
+                    },
+                })
+                .then((file) => {
+                    setUploadedFile({ ...file.data });
+                    setErrorMsg("");
+                    submitBtn.current.style.visibility = "hidden";
+                    setDisplayProgress(true);
+                })
+                .catch((err) => {
+                    const offlineError =
+                        "Error: MongooseServerSelectionError: Could not connect to any servers in your MongoDB Atlas cluster. One common reason is that you're trying to access the database from an IP that isn't whitelisted. Make sure your current IP address is on your Atlas cluster's IP whitelist: https://docs.atlas.mongodb.com/security-whitelist/";
+                    const fileSizeError = "File too large";
+                    if (err.response.data === offlineError)
+                        setErrorMsg(
+                            "Please connect to the internet and try again."
+                        );
+                    else if (err.response.data === fileSizeError)
+                        setErrorMsg(
+                            "File is too large. Please choose a file of size < 1 MB."
+                        );
+                    else setErrorMsg(err.response.data);
+                });
+        } else {
+            setErrorMsg("Please Select a File.");
         }
     };
 
-    const handleFile = (e) => {
-        setFile(e.target.files[0]);
-    };
+    const handleFile = (files) => {
+        const [uploadedFile] = files;
+        setFile(uploadedFile);
+        setDisplayLinks(false);
 
-    useEffect(() => {
-        console.log(allFiles);
-    }, [allFiles]);
+        const fileReader = new FileReader();
+        fileReader.onload = () => {
+            setPreviewSrc(fileReader.result);
+        };
+        fileReader.readAsDataURL(uploadedFile);
+
+        setIsPreviewAvailable(
+            uploadedFile.name.match(/\.(jpeg|jpg|png|webp|gif|svg)$/)
+        );
+    };
 
     const downloadLink = (id, path, mimetype) => {
         const folders = path.split("/");
         let filename = folders.pop();
-        const lastUnderScore = filename.lastIndexOf("_");
-        filename = filename.slice(lastUnderScore + 1);
+        const lastUnderScore = filename.lastIndexOf("__");
+        filename = filename.slice(lastUnderScore + 2);
         axios
             .get(`http://localhost:5000/file/download/${id}`, {
                 responseType: "blob",
@@ -73,53 +154,132 @@ const Home = (props) => {
             })
             .catch((err) => console.error(err));
     };
+
+    const handleBtnClick = (e) => {
+        navigator.clipboard.writeText(
+            `http://localhost:3000/file/download/${uploadedFile._id}`
+        );
+
+        const toolTip = document.querySelector(
+            "button.share-link .tooltiptext"
+        );
+        toolTip.style.visibility = "visible";
+        toolTip.style.opacity = "1";
+
+        window.setTimeout(() => {
+            toolTip.style.visibility = "hidden";
+            toolTip.style.opacity = "0";
+        }, 5000);
+    };
+
     return (
-        <center>
-            <form onSubmit={handleSubmit}>
-                <input
-                    name="title"
-                    value={state.title}
-                    type="text"
-                    onChange={handleChange}
-                    placeholder="Title for the file"
-                />
-                <br />
-                <input
-                    name="description"
-                    value={state.description}
-                    type="text"
-                    onChange={handleChange}
-                    placeholder="Description for the file"
-                />
-                <br />
-                <input
-                    type="file"
-                    onChange={handleFile}
-                    formEncType="multipart/form-data"
-                />
-                <br />
-                <button type="submit">Submit</button>
-            </form>
-            <br />
-            <br />
-            {allFiles.map((file, index) => {
-                const { _id, file_path, file_mimetype } = file;
-                return (
-                    <div key={index}>
-                        <a
-                            href="#/"
-                            onClick={() =>
-                                downloadLink(_id, file_path, file_mimetype)
-                            }>
-                            Download File no {index}
-                        </a>{" "}
-                        <Link to={`/file/download/${_id}`}>Shareable Link</Link>
-                        <br />
-                        <br />
+        <section className="home">
+            <p className="error-msg">{errorMsg}</p>
+            <form className="file-form" onSubmit={handleSubmit}>
+                <section className="file-upload">
+                    <Dropzone
+                        onDrop={handleFile}
+                        className="drop-file"
+                        onDragEnter={() => updateBorder("over")}
+                        onDragLeave={() => updateBorder("leave")}>
+                        {({ getRootProps, getInputProps }) => (
+                            <div
+                                {...getRootProps({ className: "drop-zone" })}
+                                ref={dropRef}>
+                                <input {...getInputProps()} />
+                                <p>
+                                    Drag and drop a file OR click here to select
+                                    a file
+                                </p>
+                                {file.name ? (
+                                    <div className="file-name">
+                                        <strong>Selected file:</strong>{" "}
+                                        {file.name}
+                                    </div>
+                                ) : (
+                                    ""
+                                )}
+                            </div>
+                        )}
+                    </Dropzone>
+
+                    <div className="image-preview-message">
+                        {previewSrc ? (
+                            isPreviewAvailable ? (
+                                <div className="image-preview">
+                                    <img
+                                        ref={previewImgRef}
+                                        className="preview-image"
+                                        src={previewSrc}
+                                        alt="Preview"
+                                    />
+                                </div>
+                            ) : (
+                                <div className="preview-message">
+                                    <p>No preview available for this file</p>
+                                </div>
+                            )
+                        ) : (
+                            <div className="preview-message">
+                                <p>
+                                    Image preview will be shown here after
+                                    selection
+                                </p>
+                            </div>
+                        )}
                     </div>
-                );
-            })}
-        </center>
+                    {progress > 0 ? (
+                        <div className="progress">
+                            <div
+                                className="determinate"
+                                style={{ width: `${progress}%` }}
+                            />
+                        </div>
+                    ) : (
+                        ""
+                    )}
+                </section>
+
+                {file.name && !displayLinks ? (
+                    <button
+                        ref={submitBtn}
+                        className="submit-btn"
+                        type="submit">
+                        Submit
+                    </button>
+                ) : (
+                    ""
+                )}
+            </form>
+
+            {uploadedFile._id && displayLinks ? (
+                <div className="final-links" ref={finalLinkRef}>
+                    <a
+                        href="#/"
+                        title="Download the file"
+                        className="link"
+                        onClick={() =>
+                            downloadLink(
+                                uploadedFile._id,
+                                uploadedFile.file_path,
+                                uploadedFile.file_mimetype
+                            )
+                        }>
+                        Download File
+                    </a>{" "}
+                    <button
+                        className="share-link link"
+                        onClick={handleBtnClick}
+                        title="Copy link to clipboard"
+                        aria-label="Click to copy link">
+                        Share File
+                        <span className="tooltiptext">Link Copied!</span>
+                    </button>
+                </div>
+            ) : (
+                ""
+            )}
+        </section>
     );
 };
 
