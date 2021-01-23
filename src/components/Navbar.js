793be87f1f72f66116df85638e032eec7c9bceba
Rajatm544 <rajatm544@gmail.com>
2021-01-23 22:04:08 +0530

    add a logo and style the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "../stylesheets/navbar.css";
+import logo from "../stylesheets/logo.png";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <span className="nav-brand">EasyShare</span>
+            <div className="nav-brand" onClick={() => window.location.reload()}>
+                EasyShare
+                <img className="logo" src={logo} alt="Logo"></img>
+            </div>
             <ul className="nav-ul">
                 <li className="nav-li">About</li>
                 <li className="nav-li">Help</li>
