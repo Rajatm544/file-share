ad3f18a92e9edd0ae7e844b04329ff3d7ae2eec6
Rajatm544 <rajatm544@gmail.com>
2021-01-25 15:50:32 +0530

    add div to help flexbox layout for responsive design

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -7,18 +7,20 @@ const NotFound = () => {
     return (
         <div className="error-page">
             <img className="err-404" src={error404} alt="404" />
-            <p className="not-found">Page Not Found.</p>
-            <p className="not-found">
-                Looks Like You've Landed on The Wrong Page!
-            </p>
-            <p className="not-found">
-                Click
-                <Link className="link-home" to="/">
-                    {" "}
-                    Here{" "}
-                </Link>
-                to Go to The Home Page.
-            </p>
+            <div className="error-text">
+                <p className="not-found">Page Not Found.</p>
+                <p className="not-found">
+                    Looks Like You've Landed on The Wrong Page!
+                </p>
+                <p className="not-found">
+                    Click
+                    <Link className="link-home" to="/">
+                        {" "}
+                        Here{" "}
+                    </Link>
+                    to Go to The Home Page.
+                </p>
+            </div>
         </div>
     );
 };
