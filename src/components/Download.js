5e13dca9f5c8744f79f2390780d013e52fd47cc2
Rajatm544 <rajatm544@gmail.com>
2021-01-26 23:18:24 +0530

    add code to use production route

--- src/components/Download.js
+++ src/components/Download.js
@@ -3,23 +3,26 @@ import axios from "axios";
 import download from "downloadjs";
 import "../stylesheets/download.css";
 
+const baseURL = process.env.BASE_URL || "http://localhost:5000";
+let frontURL = "";
+if (baseURL === "http://localhost:5000") {
+    frontURL = "http://localhost:3000";
+}
+
 const Download = (props) => {
     useEffect(() => {
         const id = props.match.params.id;
         axios
-            .get(`http://localhost:5000/file/get/${id}`)
+            .get(`${baseURL}/server/file/get/${id}`)
             .then((file) => {
                 const folders = file.data.file_path.split("/");
                 let filename = folders.pop();
                 const lastUnderScore = filename.lastIndexOf("__");
                 filename = filename.slice(lastUnderScore + 2);
                 axios
-                    .get(
-                        `http://localhost:5000/file/download/${file.data._id}`,
-                        {
-                            responseType: "blob",
-                        }
-                    )
+                    .get(`${baseURL}/server/file/download/${file.data._id}`, {
+                        responseType: "blob",
+                    })
                     .then((file) => {
                         return download(
                             file.data,
@@ -29,7 +32,7 @@ const Download = (props) => {
                     })
                     .then(() =>
                         window.setTimeout(() => {
-                            window.location.replace("http://localhost:3000/");
+                            window.location.replace(frontURL || baseURL);
                         }, 1000)
                     )
                     .catch((err) => {
@@ -39,7 +42,7 @@ const Download = (props) => {
             .catch((err) => {
                 if (err.message) {
                     alert("No such file is available in the server!");
-                    window.location.replace("http://localhost:3000/");
+                    window.location.replace(baseURL || frontURL);
                 } else {
                     console.log(JSON.stringify(err));
                 }
