dc4716e062d9bbdc57e534a672abcbe3070aeda6
Rajatm544 <rajatm544@gmail.com>
2021-01-26 23:17:34 +0530

    add code to deploy server

--- server-side/server.js
+++ server-side/server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-const multer = require("multer");
 require("dotenv").config();
 
 // configure the express app
@@ -27,7 +26,18 @@ connection.once("open", () => {
 
 // configure the routes
 const fileRouter = require("./routes/file.route");
-app.use("/file", fileRouter);
+app.use("/server/file", fileRouter);
+
+//Load the npm build package of the frontend CRA
+if (process.env.NODE_ENV === "production") {
+    // set a static folder
+    app.use(express.static("/build"));
+
+    // Provide a wildcard as a fallback for all routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+    });
+}
 
 // host the app on port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
