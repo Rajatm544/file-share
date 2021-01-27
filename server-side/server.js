27f0dab6c46ddd00c6a5c4493d0dd663b5090c3f
Rajatm544 <rajatm544@gmail.com>
2021-01-27 22:40:49 +0530

    change server file to deploy to heroku

--- server-side/server.js
+++ server-side/server.js
@@ -35,7 +35,7 @@ if (process.env.NODE_ENV === "production") {
 
     // Provide a wildcard as a fallback for all routes
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
     });
 }
 
