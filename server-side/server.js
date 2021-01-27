aa2296ba8419175ca361e2559eba7f5161d7fe0f
Rajatm544 <rajatm544@gmail.com>
2021-01-27 20:46:13 +0530

    add the build folder

--- server-side/server.js
+++ server-side/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 require("dotenv").config();
 
 // configure the express app
@@ -9,8 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 // configure the port for the server side
-const PORT =
-    process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 5000;
+const PORT = process.env.PORT || 5000;
 
 // configure the mongoose setup
 const uri = process.env.MONGO_URI;
@@ -27,6 +27,17 @@ connection.once("open", () => {
 // configure the routes
 const fileRouter = require("./routes/file.route");
 app.use("/server/file", fileRouter);
+
+//Load the npm build package of the frontend CRA
+if (process.env.NODE_ENV === "production") {
+    // set a static folder
+    app.use(express.static("/build"));
+
+    // Provide a wildcard as a fallback for all routes
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+    });
+}
 
 // host the app on port
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
