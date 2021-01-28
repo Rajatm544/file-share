3d06feb48195fec4b663c6bda654e9dfe1c0533a
Rajatm544 <rajatm544@gmail.com>
2021-01-28 20:19:04 +0530

    add the aws-s3 bucket congif using multer-s3 and change the routes accordingly

--- server-side/routes/file.route.js
+++ server-side/routes/file.route.js
@@ -1,7 +1,50 @@
 const router = require("express").Router();
-const upload = require("../multer/upload");
-const path = require("path");
+const multer = require("multer");
+const aws = require("aws-sdk");
+const multerS3 = require("multer-s3");
 let File = require("../models/file.model");
+
+const s3 = new aws.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY_ID,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+    Bucket: "easysharebucket",
+});
+
+const upload = multer({
+    // storage: multer.diskStorage({
+    //     destination(req, file, cb) {
+    //         cb(null, "./server-side/sent_files");
+    //     },
+    //     filename(req, file, cb) {
+    //         cb(null, `${new Date().getTime()}__${file.originalname}`);
+    //     },
+    // }),
+    storage: multerS3({
+        s3,
+        acl: "public-read",
+        bucket: "easysharebucket",
+        key: function (req, file, cb) {
+            cb(null, `${new Date().getTime()}__${file.originalname}`);
+        },
+    }),
+    limits: {
+        fileSize: 1000000, // maximum file size of 1 MB
+    },
+    fileFilter(req, file, cb) {
+        if (
+            !file.originalname.match(
+                /\.(jpeg|jpg|png|webp|gif|pdf|doc|docx|xls|xlsx|svg|ppt|pptx)$/
+            )
+        ) {
+            return cb(
+                new Error(
+                    "Unsupported file format, please choose a different file and retry."
+                )
+            );
+        }
+        cb(undefined, true); // continue with upload
+    },
+});
 
 // Root Route
 router.get("/", (req, res) => {
@@ -22,22 +65,22 @@ router.post(
     "/",
     upload.single("file"),
     (req, res) => {
-        const { title, description } = req.body;
-        const { path, mimetype } = req.file;
-
+        const { key, mimetype, location } = req.file;
+        const lastUnderScore = key.lastIndexOf("__");
+        const file_name = key.slice(lastUnderScore + 2);
         const file = new File({
-            title,
-            description,
-            file_path: path,
+            file_key: key,
             file_mimetype: mimetype,
+            file_location: location,
+            file_name,
         });
 
         file.save()
             .then(() => {
-                File.find({})
-                    .sort({ createdAt: -1 })
-                    .limit(1)
-                    .then((file) => res.json(file[0]))
+                File.findOne({ file_key: key })
+                    .then((file) => {
+                        res.json(file);
+                    })
                     .catch((err) => res.status(400).send(`Error: ${err}`));
             })
             .catch((err) => res.status(400).json(`Error: ${err}`));
@@ -51,26 +94,23 @@ router.post(
 
 router.get("/get/:id", (req, res) => {
     File.findById(req.params.id)
-        .then((file) => res.json(file))
+        .then((file) => {
+            res.set({
+                "Content-Type": file.file_mimetype,
+            });
+            const params = {
+                Key: file.file_key,
+                Bucket: "easysharebucket",
+            };
+            s3.getObject(params, (err, data) => {
+                if (err) {
+                    res.status(400).json(`Error: ${err}`);
+                } else {
+                    res.json({ file, data });
+                }
+            });
+        })
         .catch((err) => res.status(400).json(`Error: ${err}`));
-});
-
-router.get("/download/:id", (req, res) => {
-    try {
-        File.findById(req.params.id)
-            .then((file) => {
-                res.set({
-                    "Content-Type": file.file_mimetype,
-                });
-                res.sendFile(path.join(__dirname, "../..", file.file_path));
-            })
-            .catch((err) => res.status(400).json(`Error: ${err}`));
-    } catch (error) {
-        if (error)
-            res.status(500).send(
-                "Error while fetching this file. Please try again later."
-            );
-    }
 });
 
 router.delete("/", (req, res) => {
