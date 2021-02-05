317901cc96e23fe38ab11d32fd60bc26e1b64057
Rajatm544 <rajatm544@gmail.com>
2021-02-05 14:29:16 +0530

    add comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"; // React Router
+
+// import all the required components
 import Home from "./components/Home";
 import Download from "./components/Download";
 import Navbar from "./components/Navbar";
@@ -14,9 +16,13 @@ const App = () => {
             <Router>
                 <Navbar />
                 <Switch>
+                    {/* Home page component */}
                     <Route path="/" exact component={Home} />
+                    {/* The donwload page needs to have the id of the file to be downloaded in its params */}
                     <Route path="/download/:id" exact component={Download} />
+                    {/* The about page */}
                     <Route path="/about" exact component={About} />
+                    {/* A catch-all page to display 404-error */}
                     <Route component={NotFound} />
                 </Switch>
             </Router>
